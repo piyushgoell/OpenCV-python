c5311d0d2e673eac318d91d817c8b71a78b25de3
Piyush Goel <34750712+piyushgoell@users.noreply.github.com>
2019-01-13 19:20:25 +0530

    Defining function for Aruco Generation

--- ArucoGeneration.py
+++ ArucoGeneration.py
@@ -1,7 +1,17 @@
 import cv2
 import cv2.aruco as aruco
-aruco_dict = aruco.Dictionary_get(aruco.DICT_7X7_1000)
-img = aruco.drawMarker(aruco_dict,id_aruco,num_pixels)
-cv2.imshow('ArUcoID'+str(id_aruco),img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+def aruco_generation(id_aruco):
+  aruco_dict = aruco.Dictionary_get(aruco.DICT_7X7_1000)
+  img = aruco.drawMarker(aruco_dict,id_aruco,num_pixels)
+  cv2.imshow('ArUcoID'+str(id_aruco),img)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+ 
+if __name__ == "__main__":
+  aruco_generation(78, 500)
+  
+    
+  
+  
+
