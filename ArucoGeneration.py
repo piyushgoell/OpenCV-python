cbcba17e123e2760cfe3cb415eea92c23c5eb471
piyushgoell <34750712+piyushgoell@users.noreply.github.com>
2019-01-13 18:56:20 +0530

    Create ArucoGeneration.py

---
+++ ArucoGeneration.py
@@ -0,0 +1,7 @@
+import cv2
+import cv2.aruco as aruco
+aruco_dict = aruco.Dictionary_get(aruco.DICT_7X7_1000)
+img = aruco.drawMarker(aruco_dict,id_aruco,num_pixels)
+cv2.imshow('ArUcoID'+str(id_aruco),img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
