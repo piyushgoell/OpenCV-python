8fc4d920cd7419fdceed0b8d5a7885e6b2c1e3a1
Piyush Goel <34750712+piyushgoell@users.noreply.github.com>
2019-03-30 02:36:13 +0530

    Adding comments and few other things

--- ArUcoDetection.py
+++ ArUcoDetection.py
@@ -1,18 +1,40 @@
+##################################################################################################################################
+######################    Python code for Detecting any ArUco Id of any Dictionary    ###########################################
+##################################################################################################################################
+
 import cv2
 import cv2.aruco as aruco
 
-aruco_list = {}
-img = cv2.imread('Image2.jpg',1) 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_7X7_1000)
-#print(aruco_dict)
-parameters = aruco.DetectorParameters_create()
-#print(parameters)
-corners, ids, _ = aruco.detectMarkers(gray, aruco_dict, parameters = parameters)
-#print(corners)
-print(ids)
-gray = aruco.drawDetectedMarkers(gray, corners,ids)
+##################################################################################################################################
+###############################         Function for Detecting ArucoID         ##################################################
+##################################################################################################################################
+def aruco_detection(img):
+  
+  # Creating a empty list to store the values 
+  aruco_list = {}
+  
+  # Conterting Image to GRAYSCALE
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  
+  # This function returns one of the predefined dictionaries referenced by DICT_*
+  # aruco.Dictionary_get(Dictionary)
+  # Change as per the requirement (aruco.DICT_NxN_Combinations) for generating Aruco Id's of Different Dictionaries
+  aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_7X7_1000)
 
-cv2.imshow('frame',img) 
-cv2.waitKey(0)
-cv2.destroyAllWindows() 
+  # This Function performs marker detection in the input image. 
+  # Only markers included in the dictionary specificed above are searched.
+  corners, ids, _ = aruco.detectMarkers(gray, aruco_dict)
+  print(ids)
+  # This function returns a marker image in its canonical form (i.e. ready to be printed)
+  aruco.drawDetectedMarkers( img, corners, ids)
+  
+  # Displaying the Result
+  cv2.imshow('frame',img)
+
+  
+if __name__ == "__main__":
+# Reading the image  
+  img = cv2.imread('Images/ArUco.jpg',1) 
+  aruco_detection(img)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows() 
